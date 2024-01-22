e070901674bbc6b48141e528c5fcd127aac21a1c
OlgaBogush <bohush.olga@gmail.com>
2024-01-22 13:36:18 +0300

    init: start html-builder add code for 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const readableStream = fs.createReadStream(path.resolve(__dirname, 'text.txt'), 'utf-8');
+let data = '';
+readableStream.on('data', (chunk) => (data += chunk));
+readableStream.on('end', () => console.log(data));
+readableStream.on('error', (error) => console.log('It is not possible to find the file "text.txt"!', error.message));
